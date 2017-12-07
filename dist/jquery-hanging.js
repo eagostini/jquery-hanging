1c6e4ef451e6d3084dde6e9f93ad3eb3663e4d1f
Eduardo Agostini <edu.agostini@gmail.com>
2017-12-08 12:26:59 +1300

    Events
    We need `onchange`…

--- dist/jquery-hanging.js
+++ dist/jquery-hanging.js
@@ -14,7 +14,7 @@
         it: function it(target, defaults) {
             var that = this;
 
-            return target.on('input.hanging', function (event) {
+            return target.on('change.hanging input.hanging', function (event) {
                 var target = $(this);
 
                 window.setTimeout(function () {
